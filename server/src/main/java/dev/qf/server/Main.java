9234580fcf634de889038306757d277685af8451
biryeongtrain <83863154+biryeongtrain@users.noreply.github.com>
2025-05-18 20:32:01 +0900

    feat : add debug log

--- server/src/main/java/dev/qf/server/Main.java
+++ server/src/main/java/dev/qf/server/Main.java
@@ -1,7 +1,10 @@
 package dev.qf.server;
 
+import common.KioskLoggerFactory;
+
 public class Main {
     public static void main(String[] args) {
         KioskNettyServer.run();
+        KioskLoggerFactory.getLogger().info("Server started");
     }
 }
