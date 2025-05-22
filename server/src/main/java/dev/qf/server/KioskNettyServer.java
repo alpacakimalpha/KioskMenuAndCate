2b048a032249778abfe961fd32ac30e5c64469c1
biryeongtrain <biryeongtrain06@gmail.com>
2025-05-22 11:20:56 +0900

    싱글톤 구현하고 셧다운 로직을 메소드로 뻈음.

--- server/src/main/java/dev/qf/server/KioskNettyServer.java
+++ server/src/main/java/dev/qf/server/KioskNettyServer.java
@@ -1,41 +1,48 @@
 package dev.qf.server;
 
-import com.google.common.base.Suppliers;
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import common.KioskLoggerFactory;
+import common.network.Connection;
 import common.network.SerializableManager;
 import common.network.handler.SerializableDecoder;
 import common.network.handler.SerializableEncoder;
 import common.network.handler.SerializableHandler;
 import common.network.packet.SidedPacket;
+import common.util.Container;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioIoHandler;
-import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
-import io.netty.handler.timeout.ReadTimeoutHandler;
 import org.slf4j.Logger;
 
-import java.util.function.Supplier;
-
-public class KioskNettyServer {
+public class KioskNettyServer implements Connection {
     //TODO MOVE TO CONFIG
     private static final int port = 8192;
-    private static final Supplier<MultiThreadIoEventLoopGroup> CHANNEL = Suppliers.memoize(() -> new MultiThreadIoEventLoopGroup(0, new ThreadFactoryBuilder().setDaemon(true).setNameFormat("Netty IO #%d").build(), NioIoHandler.newFactory()));
+    private final MultiThreadIoEventLoopGroup CHANNEL = new MultiThreadIoEventLoopGroup(NioIoHandler.newFactory());
+    private final MultiThreadIoEventLoopGroup WORKER = new MultiThreadIoEventLoopGroup(NioIoHandler.newFactory());
     public static final Logger LOGGER = KioskLoggerFactory.getLogger();
+    private Channel channel;
 
-    public static void run() {
+    KioskNettyServer() {
+        if (Main.INSTANCE != null) {
+            throw new IllegalStateException("Server is already started");
+        }
+        Container.put(Connection.class, this);
+    }
+
+    @Override
+    public void run() {
         try {
             ServerBootstrap serverBootstrap = new ServerBootstrap();
-            serverBootstrap.group(CHANNEL.get());
+            serverBootstrap.group(CHANNEL, WORKER);
             serverBootstrap.channel(NioServerSocketChannel.class);
             SerializableManager.initialize();
 
-            serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+            serverBootstrap.handler(new LoggingHandler(LogLevel.INFO));
+            serverBootstrap.childHandler(new ChannelInitializer<Channel>() {
                 @Override
-                protected void initChannel(SocketChannel channel) {
+                protected void initChannel(Channel channel) {
                     try {
                         channel.config().setOption(ChannelOption.TCP_NODELAY, true);
                         channel.config().setOption(ChannelOption.SO_KEEPALIVE, true);
@@ -44,15 +51,12 @@ public class KioskNettyServer {
                     }
 
                     ChannelPipeline pipeline = channel.pipeline()
-                            .addLast("timeout", new ReadTimeoutHandler(30))
+//                            .addLast("timeout", new ReadTimeoutHandler(30))
                             ;
 
-//                    pipeline.addLast(new LineBasedFrameDecoder(1024));
-                    pipeline.addLast("logger", new LoggingHandler(LogLevel.INFO));
+                    pipeline.addLast("serializable_decoder", new SerializableDecoder());
                     pipeline.addLast("encoder", new SerializableEncoder());
                     pipeline.addLast("handler", new SerializableHandler(SidedPacket.Side.SERVER));
-                    pipeline.addLast("serializable_decoder", new SerializableDecoder());
-
                 }
 
 
@@ -61,8 +65,35 @@ public class KioskNettyServer {
         } catch (Exception e) {
             LOGGER.error("Failed to start server.");
             LOGGER.error(e.getMessage());
+        } finally {
         }
     }
 
+    public void shutdown() {
+        LOGGER.info("Shutting down client...");
+        if (channel != null && channel.isOpen()) {
+            try {
+                channel.close().syncUninterruptibly();
+            } catch (Exception e) {
+                LOGGER.warn("Exception while closing client channel", e);
+            }
+        }
+        if (!CHANNEL.isShuttingDown() && !CHANNEL.isShutdown()) {
+            CHANNEL.shutdownGracefully().syncUninterruptibly();
+        }
+        if (!WORKER.isShuttingDown() && !WORKER.isShutdown()) {
+            WORKER.shutdownGracefully().syncUninterruptibly();
+        }
+        LOGGER.info("Client shutdown complete.");
+    }
 
+    @Override
+    public void setChannel(Channel channel) {
+        this.channel = channel;
+    }
+
+    @Override
+    public Channel getChannel() {
+        return this.channel;
+    }
 }
