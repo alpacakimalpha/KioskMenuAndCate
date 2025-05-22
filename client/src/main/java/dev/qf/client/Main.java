f60d309cd7dc28ac6f30bc7193516e0a7efc2c86
biryeongtrain <biryeongtrain06@gmail.com>
2025-05-22 11:19:38 +0900

    fix : 각 Connection을 싱글턴으로 변경. 사실 Container 에서도 가져올 수 있는데, Initialize 될 때 Container에도 넣기때문에 사실상 둘다 같은 인스턴스임. 다만 수정은 필요해 보임

--- client/src/main/java/dev/qf/client/Main.java
+++ client/src/main/java/dev/qf/client/Main.java
@@ -1,6 +1,10 @@
 package dev.qf.client;
 
+import common.network.Connection;
+import dev.qf.client.network.KioskNettyClient;
+
 public class Main {
+    public static Connection INSTANCE = new KioskNettyClient();
     public static void main(String[] args) {
 
     }
--- server/src/main/java/dev/qf/server/Main.java
+++ server/src/main/java/dev/qf/server/Main.java
@@ -1,10 +1,13 @@
 package dev.qf.server;
 
 import common.KioskLoggerFactory;
+import common.network.Connection;
 
 public class Main {
+    public static Connection INSTANCE = new KioskNettyServer();
     public static void main(String[] args) {
-        KioskNettyServer.run();
+
+        INSTANCE.run();
         KioskLoggerFactory.getLogger().info("Server started");
     }
 }
