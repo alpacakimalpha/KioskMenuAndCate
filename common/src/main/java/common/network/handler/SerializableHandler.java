4f902fe804032be08d370ca403a0c844135d3162
biryeongtrain <biryeongtrain06@gmail.com>
2025-05-22 11:21:28 +0900

    디버깅용 로직 추가

--- common/src/main/java/common/network/handler/SerializableHandler.java
+++ common/src/main/java/common/network/handler/SerializableHandler.java
@@ -1,31 +1,29 @@
 package common.network.handler;
 
-import com.google.gson.Gson;
 import common.KioskLoggerFactory;
 import common.network.Connection;
-import common.network.Serializable;
 import common.network.packet.SidedPacket;
+import common.util.Container;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import org.slf4j.Logger;
 
 public class SerializableHandler extends SimpleChannelInboundHandler<SidedPacket> {
     private static final Logger LOGGER = KioskLoggerFactory.getLogger();
-    private static final Gson gson = new Gson();
 
     public SerializableHandler(SidedPacket.Side side) {
         this.side = side;
     }
 
     private final SidedPacket.Side side;
+
     @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, SidedPacket s) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, SidedPacket s) throws Exception {
+        LOGGER.info(s.toString());
         if (s.getSide() == this.side) { // 이게 그렇게 쓸모 있는 코드가 아닌 것 같지만 아무튼.
             s.apply();
         }
     }
-
-
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
@@ -36,5 +34,6 @@ public class SerializableHandler extends SimpleChannelInboundHandler<SidedPacket
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         super.channelActive(ctx);
+        Container.get(Connection.class).setChannel(ctx.channel());
     }
 }
