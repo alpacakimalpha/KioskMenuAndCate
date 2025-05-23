72bede365124d087a1417375fad6e3af21635073
biryeongtrain <biryeongtrain06@gmail.com>
2025-05-23 14:13:35 +0900

    테스트 코드 메인코드 변경사항 반영 및 수정

--- client/src/test/java/NetworkConnectionTest.java
+++ client/src/test/java/NetworkConnectionTest.java
@@ -1,9 +1,5 @@
-import common.network.packet.HandShakePacket;
-import common.network.packet.HandShakeS2CInfo;
+import common.network.packet.HandShakeC2SInfo;
 import dev.qf.client.Main;
-import dev.qf.client.network.KioskNettyClient;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
 import org.junit.jupiter.api.Assertions;
 import org.testng.annotations.Test;
 
@@ -11,14 +7,12 @@ public class NetworkConnectionTest {
     @Test
     public void testNetworkConnection() throws InterruptedException {
         Main.INSTANCE.run();
-        var future = Main.INSTANCE.sendSerializable(new HandShakePacket(new HandShakeS2CInfo("test")));
+        var future = Main.INSTANCE.sendSerializable(new HandShakeC2SInfo("test"));
 
         while(!future.isDone()) {
             Thread.sleep(100);
         }
         Assertions.assertTrue(future.isSuccess());
-        //        ChannelFuture future = KioskNettyClient.connect("127.0.0.1", 8192);
-//        future.channel().writeAndFlush("Hello World");
 
         Main.INSTANCE.shutdown();
     }
