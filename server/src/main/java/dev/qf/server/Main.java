256b9cd1da3bec13e36d0509c5afdfcbfb7e642d
biryeongtrain <biryeongtrain06@gmail.com>
2025-05-23 14:12:46 +0900

    Main KioskNetworkHandler 패키지 변경 반영

--- server/src/main/java/dev/qf/server/Main.java
+++ server/src/main/java/dev/qf/server/Main.java
@@ -2,6 +2,7 @@ package dev.qf.server;
 
 import common.KioskLoggerFactory;
 import common.network.Connection;
+import dev.qf.server.network.KioskNettyServer;
 
 public class Main {
     public static Connection INSTANCE = new KioskNettyServer();
