492315a29b06a06328f7facbd8ee830f2590fccd
biryeongtrain <biryeongtrain06@gmail.com>
2025-05-26 15:11:57 +0900

    팩토리 싱글톤 등록 로직 추가

--- server/src/main/java/dev/qf/server/Main.java
+++ server/src/main/java/dev/qf/server/Main.java
@@ -2,13 +2,18 @@ package dev.qf.server;
 
 import common.KioskLoggerFactory;
 import common.network.Connection;
+import common.network.handler.PacketListener;
+import common.network.handler.factory.PacketListenerFactory;
+import common.util.Container;
 import dev.qf.server.network.KioskNettyServer;
+import dev.qf.server.network.ServerPacketListenerFactory;
 
 public class Main {
     public static Connection INSTANCE = new KioskNettyServer();
     public static void main(String[] args) {
+        KioskLoggerFactory.getLogger().info("Server started");
+        Container.put(PacketListenerFactory.class, new ServerPacketListenerFactory());
 
         INSTANCE.run();
-        KioskLoggerFactory.getLogger().info("Server started");
     }
 }
