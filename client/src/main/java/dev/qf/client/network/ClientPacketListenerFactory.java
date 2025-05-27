505e0d29d6ab231e0d482141130a384e52bbdefc
biryeongtrain <biryeongtrain06@gmail.com>
2025-05-27 21:43:36 +0900

    PacketListener 가 Handler 를 요구하도록 변경

--- client/src/main/java/dev/qf/client/network/ClientPacketListenerFactory.java
+++ client/src/main/java/dev/qf/client/network/ClientPacketListenerFactory.java
@@ -1,13 +1,12 @@
 package dev.qf.client.network;
 
-import common.network.handler.PacketListener;
-import common.network.handler.client.ClientPacketListener;
+import common.network.handler.listener.PacketListener;
+import common.network.handler.SerializableHandler;
 import common.network.handler.factory.PacketListenerFactory;
-import io.netty.channel.Channel;
 
 public class ClientPacketListenerFactory implements PacketListenerFactory {
     @Override
-    public PacketListener getPacketListener(Channel channel) {
+    public PacketListener getPacketListener(SerializableHandler channel) {
         return new ClientPacketListenerImpl(channel);
     }
 }
--- server/src/main/java/dev/qf/server/network/ServerPacketListenerFactory.java
+++ server/src/main/java/dev/qf/server/network/ServerPacketListenerFactory.java
@@ -1,13 +1,12 @@
 package dev.qf.server.network;
 
-import common.network.handler.PacketListener;
+import common.network.handler.listener.PacketListener;
+import common.network.handler.SerializableHandler;
 import common.network.handler.factory.PacketListenerFactory;
-import common.network.handler.server.ServerPacketListener;
-import io.netty.channel.Channel;
 
 public class ServerPacketListenerFactory implements PacketListenerFactory {
     @Override
-    public PacketListener getPacketListener(Channel channel) {
-        return new ServerPacketListenerImpl(channel);
+    public PacketListener getPacketListener(SerializableHandler handler) {
+        return new ServerPacketListenerImpl(handler);
     }
 }
