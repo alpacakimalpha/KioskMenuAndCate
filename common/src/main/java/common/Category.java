67126016ad096cf7490d7812c67178098aaef2a0
biryeongtrain <biryeongtrain06@gmail.com>
2025-05-27 15:09:48 +0900

    remove getter method

--- common/src/main/java/common/Category.java
+++ common/src/main/java/common/Category.java
@@ -3,7 +3,4 @@ package common;
 import java.util.List;
 
 public record Category(String cateId, String cateName, List<Menu> menuList) {
-    public String getcateId() { return cateId; }
-    public String getcateName() { return cateName; }
-    public List<Menu> menuList() { return menuList; }
 }
