0ede9ad0c17aad4afaf846328139ea214d2a95da
biryeongtrain <biryeongtrain06@gmail.com>
2025-05-28 17:37:11 +0900

    패킷 추가 : HelloS2CPacket  - 해당 패킷은 클라이언트에서 handshake 를 보낼 때 그에 대한 피드백으로 전달됨  - 해당 패킷에서는 nonce와 public key 가 있으며, 클라이언트는 접속 유지를 위해 해당 public key 와 nonce 를 그대로 반환해야함.

---
+++ common/src/main/java/common/network/packet/HelloS2CPacket.java
@@ -0,0 +1,76 @@
+package common.network.packet;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import common.network.encryption.NetworkEncryptionUtils;
+import common.network.handler.listener.ClientPacketListener;
+import common.util.JavaCodecs;
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NotNull;
+
+import java.security.PublicKey;
+
+public final class HelloS2CPacket implements SidedPacket<ClientPacketListener> {
+    public static final Codec<HelloS2CPacket> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+                    JavaCodecs.BYTE_ARRAY.fieldOf("public_key").forGetter(HelloS2CPacket::getPublicKeyAsByte),
+                    JavaCodecs.BYTE_ARRAY.fieldOf("nonce").forGetter(HelloS2CPacket::getNonceAsByte)
+            ).apply(instance, HelloS2CPacket::new)
+    );
+    private final byte[] publicKey;
+    private final byte[] nonce;
+    private volatile Byte[] nonceAsByte;
+    private volatile Byte[] publicKeyAsByte;
+
+    public HelloS2CPacket(Byte[] publicKey, Byte[] nonce) {
+        this.publicKey = JavaCodecs.asByteArray(publicKey);
+        this.nonce = JavaCodecs.asByteArray(nonce);
+    }
+
+    @Override
+    public Side getSide() {
+        return Side.CLIENT;
+    }
+
+    @Override
+    public void apply(ClientPacketListener listener) {
+        listener.onHello(this);
+    }
+
+    public PublicKey getPublicKey() {
+        return NetworkEncryptionUtils.decodeEncodedRsaPublicKey(this.publicKey);
+    }
+
+    @Override
+    public String getPacketId() {
+        return "hello_s2c_packet";
+    }
+
+    @Override
+    public @NotNull Codec<? extends SidedPacket> getCodec() {
+        return CODEC;
+    }
+
+    @ApiStatus.Internal
+    private Byte[] getNonceAsByte() {
+        if (nonceAsByte == null) {
+            this.nonceAsByte = JavaCodecs.asBoxingByteArray(nonce);
+        }
+        return nonceAsByte;
+    }
+
+    @ApiStatus.Internal
+    private Byte[] getPublicKeyAsByte() {
+        if (publicKeyAsByte == null) {
+            this.publicKeyAsByte = JavaCodecs.asBoxingByteArray(publicKey);
+        }
+        return publicKeyAsByte;
+    }
+
+    public byte[] publicKey() {
+        return publicKey;
+    }
+
+    public byte[] nonce() {
+        return nonce;
+    }
+}
