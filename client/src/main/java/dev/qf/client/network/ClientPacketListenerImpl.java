e20613ded7229e1b065357d818e1c42e3d53fa3e
biryeongtrain <biryeongtrain06@gmail.com>
2025-05-28 17:41:39 +0900

    onHello 구현. Javadoc 숙지하고 해당 부분 핸들링을 해야함.

--- client/src/main/java/dev/qf/client/network/ClientPacketListenerImpl.java
+++ client/src/main/java/dev/qf/client/network/ClientPacketListenerImpl.java
@@ -1,15 +1,41 @@
 package dev.qf.client.network;
 
-import common.network.handler.client.ClientPacketListener;
+import common.network.encryption.NetworkEncryptionUtils;
+import common.network.handler.SerializableHandler;
+import common.network.handler.listener.ClientPacketListener;
+import common.network.packet.HelloS2CPacket;
 import common.network.packet.SidedPacket;
 import common.network.packet.UpdateDataPacket;
-import io.netty.channel.Channel;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import java.security.PublicKey;
 
 public class ClientPacketListenerImpl implements ClientPacketListener {
-    private Channel channel;
+    private SerializableHandler handler;
 
-    public ClientPacketListenerImpl(Channel channel) {
-        this.channel = channel;
+    public ClientPacketListenerImpl(SerializableHandler channel) {
+        this.handler = channel;
+    }
+
+    /**
+     * Client가 처음으로 Server 와 연결 된 이후, Client 가 Server에게 HandShakeC2SPacket 을 보낼 떄 서버가 callback 으로 전달하는 패킷이다.<br>
+     * 해당 패킷에는 서버의 public 키가 암호화 된 상태로 들어있다. <br>
+     * 또한 nonce 역시 존재한다. Client 에서는 nonce를 사용할 일이 딱히 없으나, 클라이언트가 서버로 같은 nonce 값을 보내지 않으면 연결이 거절된다.
+     * @param packet
+     */
+    @Override
+    public void onHello(HelloS2CPacket packet) {
+        SecretKey secretKey = NetworkEncryptionUtils.generateSecretKey();
+        PublicKey publicKey = packet.getPublicKey();
+
+        Cipher encrpytionCipher = NetworkEncryptionUtils.cipherFromKey(1, secretKey);
+        Cipher decryptionCipher = NetworkEncryptionUtils.cipherFromKey(2, secretKey);
+
+        this.handler.channel.writeAndFlush(null); // TODO IMPLEMENT SEND PUBLIC KEY
+        this.handler.encrypt(encrpytionCipher, decryptionCipher);
+
+
     }
 
     @Override
@@ -23,7 +49,7 @@ public class ClientPacketListenerImpl implements ClientPacketListener {
     }
 
     @Override
-    public Channel getChannel() {
-        return this.channel;
+    public SerializableHandler getHandler() {
+        return this.handler;
     }
 }
