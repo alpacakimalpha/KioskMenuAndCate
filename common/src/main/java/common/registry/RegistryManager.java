dd5baec9b9aa1b1b3c1b588ba63767cecca4e19f
biryeongtrain <biryeongtrain06@gmail.com>
2025-05-28 17:38:25 +0900

    레지스트리 추가. javadoc 에 있는 order 꼭 확인하고 개발할 것

--- common/src/main/java/common/registry/RegistryManager.java
+++ common/src/main/java/common/registry/RegistryManager.java
@@ -1,8 +1,24 @@
 package common.registry;
 
+import common.Category;
+import common.Menu;
+import common.Option;
+import common.OptionGroup;
+
+/**
+ * Serializable 순서
+ * 1. Options
+ * 2. OptioGroups
+ * 3. MENUS
+ * 4. CATEGORIES
+ */
 public class RegistryManager {
     public static Registry<?> getAsId(String id) {
         // TODO : IMPLEMENTATION
         return null;
     }
+    public static final Registry<Option> OPTIONS;
+    public static final Registry<OptionGroup> OPTION_GROUPS;
+    public static final Registry<Menu> MENUS = new SimpleRegistry<>("menus", Menu.SYNC_CODEC);
+    public static final Registry<Category> CATEGORIES = new SimpleRegistry<>("categories", Category.SYNC_CODEC);
 }
