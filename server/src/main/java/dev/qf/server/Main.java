c6f0ed3c51c62ab3cfd46082e3b413589dcc4e22
biryeongtrain <biryeongtrain06@gmail.com>
2025-05-29 21:27:36 +0900

    HelloS2CPacket, KeyC2SPacket 구현, LoggerFactory 위치 변경  - HelloS2CPacket은 서버가 클라이언트에게 보안정보를 넘기는 부분이다. 서버는 클라이언트에게 encoded publickey 와 nonce를 보낸다.  - Client는 이러한 publicKey를 통해 SecretKey를 만들고, PublicKey를 통해 nonce와 Secretkey를 암호화해서 전달한다.  - 이 전달 때 KeyC2SPacket 이 전달되고, 서버는 이를 PrivateKey로 복호화 해 암호화를 수행한다.

--- server/src/main/java/dev/qf/server/Main.java
+++ server/src/main/java/dev/qf/server/Main.java
@@ -1,6 +1,6 @@
 package dev.qf.server;
 
-import common.KioskLoggerFactory;
+import common.util.KioskLoggerFactory;
 import common.network.Connection;
 import common.network.handler.factory.PacketListenerFactory;
 import common.util.Container;
@@ -10,6 +10,8 @@ import dev.qf.server.network.ServerPacketListenerFactory;
 public class Main {
     public static Connection INSTANCE = new KioskNettyServer();
     public static void main(String[] args) {
+
+
         KioskLoggerFactory.getLogger().info("Server started");
         Container.put(PacketListenerFactory.class, new ServerPacketListenerFactory());
 
