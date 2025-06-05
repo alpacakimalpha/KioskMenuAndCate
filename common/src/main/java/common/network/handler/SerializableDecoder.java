6ad4d555e2e30d093ced3f2d78f380414568cc24
biryeongtrain <83863154+biryeongtrain@users.noreply.github.com>
2025-06-05 09:55:14 +0900

    코드 클린업, 마크다운 작성

--- common/src/main/java/common/network/handler/SerializableDecoder.java
+++ common/src/main/java/common/network/handler/SerializableDecoder.java
@@ -26,35 +26,38 @@ import java.util.List;
 public class SerializableDecoder extends ByteToMessageDecoder {
     private static final Logger LOGGER = KioskLoggerFactory.getLogger();
     public static final Gson gson = new Gson();
+
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
         int i = in.readableBytes();
-        if (i != 0) {
-            String msg;
-            try {
-                msg = StringEncodings.decode(in, 65535);
-            } catch (Exception e) {
-                LOGGER.error(e.getMessage(), e);
-                return;
-            }
-            JsonObject jsonObject = gson.fromJson(msg, JsonObject.class);
-            String type = JsonHelper.getString(jsonObject, Serializable.PACKET_ID_PROPERTY);
+        if (i == 0) {
+            return;
+        }
 
-            if (in.readableBytes() > 0) {
-                throw new IOException(
-                        "Packet " +
-                        type +
-                        " was larger than expected; found " +
-                        in.readableBytes() +
-                        " extra bytes while reading the packet."
-                );
-            } else {
-                Codec<?> codec = SerializableManager.getCodec(type).orElseThrow(() -> new IllegalArgumentException("Unknown packet type: " + type));
-                out.add(codec.decode(JsonOps.INSTANCE, jsonObject.get(Serializable.DATA_PROPERTY)).getOrThrow().getFirst());
+        String msg;
+        try {
+            msg = StringEncodings.decode(in, 32767);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+            return;
+        }
+        JsonObject jsonObject = gson.fromJson(msg, JsonObject.class);
+        String type = JsonHelper.getString(jsonObject, Serializable.PACKET_ID_PROPERTY);
+
+        if (in.readableBytes() > 0) {
+            throw new IOException(
+                    "Packet " +
+                            type +
+                            " was larger than expected; found " +
+                            in.readableBytes() +
+                            " extra bytes while reading the packet."
+            );
+        } else {
+            Codec<?> codec = SerializableManager.getCodec(type).orElseThrow(() -> new IllegalArgumentException("Unknown packet type: " + type));
+            out.add(codec.decode(JsonOps.INSTANCE, jsonObject.get(Serializable.DATA_PROPERTY)).getOrThrow().getFirst());
 
 
-                LOGGER.info(SerializableManager.SERIALIZABLE_RECEIVED_MARKER, "IN [{}] -> {} bytes", type, i);
-            }
+            LOGGER.info(SerializableManager.SERIALIZABLE_RECEIVED_MARKER, "IN [{}] -> {} bytes", type, i);
         }
     }
 }
